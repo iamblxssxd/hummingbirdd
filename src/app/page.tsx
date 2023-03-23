76b99ab41a7ff033454c2daa1b6c20e11ccbb85b
blxssxd <iamblxssxd@gmail.com>
2023-03-22 20:45:33 -0400

    Create textarea ui component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Textarea } from '@/components/ui/Textarea'
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <>
+      <div className='flex flex-col justify-start max-w-4xl mx-auto'>
+        <h1 className='font-bold text-3xl md:text-4xl'>Your text:</h1>
+        <div className='pt-12'>
+          <Textarea />
+        </div>
+      </div>
+    </>
+  )
 }
