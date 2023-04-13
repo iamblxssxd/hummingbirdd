12e842be47af901ebe873b378617436454af3fbb
blxssxd <iamblxssxd@gmail.com>
2023-04-13 19:22:11 -0400

    Finish signin page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link'
+import { Icons } from '@/components/Icons'
+
+import { cn } from '@/lib/utils'
+
+import { UserAuthForm } from '@/components/UserAuthForm'
+import WordTooltip from '@/components/WordTooltip'
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className='container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
+        <div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex'>
+          <div className='absolute inset-0 bg-zinc-900' />
+          <div className='relative z-20 flex items-center text-lg font-medium'>
+            <Icons.logo className='mr-2 h-6 w-6' />
+          </div>
+          <div className='relative z-20 mt-auto'>
+            <blockquote className='space-y-2'>
+              <p className='text-lg'>
+                &ldquo;This app has given me{' '}
+                <WordTooltip word='countless' definition='having very many' />{' '}
+                hours of joy while reading my favorite articles and expanded my
+                vocabulary by a mile!&rdquo;
+              </p>
+              <footer className='text-sm'>Kathy Griffin</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className='lg:p-8'>
+          <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+            <div className='flex flex-col space-y-2 text-center'>
+              <h1 className='text-2xl font-semibold tracking-tight'>
+                Create an account
+              </h1>
+              <p className='text-sm text-muted-foreground'>
+                Enter your email below to create your account
+              </p>
+            </div>
+            <UserAuthForm />
+            <p className='px-8 text-center text-sm text-muted-foreground'>
+              By clicking continue, you agree to our{' '}
+              <Link
+                href='/terms'
+                className='underline underline-offset-4 hover:text-primary'>
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link
+                href='/privacy'
+                className='underline underline-offset-4 hover:text-primary'>
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
