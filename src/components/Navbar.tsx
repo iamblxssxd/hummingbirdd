eeb50ae778edbcb2ec9be2b1fb00e1b6a4e93e1d
blxssxd <iamblxssxd@gmail.com>
2023-04-14 20:15:28 -0400

    Fix nav icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ import ThemeToggle from '@/components/ThemeToggle'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 inset-x-0 h-fit  z-[10] py-2'>
-      <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
+    <div className='fixed top-0 inset-x-0 h-full  z-[10] py-2'>
+      <div className='container max-w-7xl h-14 mx-auto flex items-center justify-between gap-2'>
         <Link href='/' className='flex gap-2 items-center'>
-          <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
+          <Icons.logo className='h-6 w-6' />
           <p className='hidden text-md font-medium md:block'>Hummingbird</p>
         </Link>
 
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -15,11 +15,11 @@ const ThemeToggle = () => {
       size='icon'
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
       <Icons.sun
-        className='h-8 w-8 sm:h-6 sm:w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 '
+        className='h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'
         aria-hidden='true'
       />
       <Icons.moon
-        className='absolute h-8 w-8 sm:h-6 sm:w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100'
+        className='absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100'
         aria-hidden='true'
       />
       <span className='sr-only'>Toggle theme</span>
