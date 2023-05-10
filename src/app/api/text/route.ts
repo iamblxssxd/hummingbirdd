1919de52641ec87a5e1b201f292f6c3a9c0c5b90
blxssxd <iamblxssxd@gmail.com>
2023-05-09 20:44:13 -0400

    Create api route for text

---
+++ src/app/api/text/route.ts
@@ -0,0 +1,53 @@
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { TextValidator } from '@/lib/validators/text'
+import { z } from 'zod'
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession()
+
+    if (!session?.user) {
+      return new Response('Unauthorized', { status: 401 })
+    }
+
+    const body = await req.json()
+    const { text } = TextValidator.parse(body)
+
+    // if text exists on the currently logged in user
+    const textExists = await db.user.findFirst({
+      where: {
+        id: session.user.id,
+        texts: {
+          some: {
+            content: {
+              equals: text,
+            },
+          },
+        },
+      },
+    })
+
+    if (textExists) {
+      return new Response('This text already has been saved', { status: 409 })
+    }
+    // TODO redirect user to the text
+
+    const newText = await db.text.create({
+      data: {
+        content: text,
+        userId: session.user.id,
+      },
+    })
+
+    return new Response(newText.content)
+  } catch (error) {
+    console.error('An error occured:', error)
+
+    if (error instanceof z.ZodError) {
+      return new Response(error.message, { status: 422 })
+    }
+
+    return new Response('Could not save text', { status: 500 })
+  }
+}
